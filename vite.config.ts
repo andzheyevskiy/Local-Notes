30897b83f619c8f69cf0a878b249307d0be29193
andzheyevskiy <Igor_lifeking@hotmail.com>
2024-09-06 13:43:49 +0200

    (Configurations)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: path.join(__dirname, "dist"),
-  }
+  base: './'
 })
+
